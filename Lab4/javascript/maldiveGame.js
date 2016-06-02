2bf3a03dad4696080362d5feffc8f95188b39112
Thornfalk <Thorn.falk@gmail.com>
2016-06-02 17:54:47 +0200

    Worked on Maldive Game and added photos, canvas and javascript. Sucessfully linked all pages

---
+++ Lab4/javascript/maldiveGame.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    drawCanvas();
+});
+function drawCanvas() {
+    var canvas = document.getElementById('game');
+    var context = canvas.getContext('2d');
+    context.strokeStyle = 'gold';
+    context.strokeRect(20, 20, 150, 100);
+    context.fillStyle = 'lightblue';
+    context.fillRect(20, 20, 150, 100);
+}
